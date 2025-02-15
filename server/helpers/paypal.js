c12f80ced2829ae6c9b4a1b1bff61213fe5cff11
k-iishan <kk1085035@gmail.com>
2025-02-15 21:50:28 +0530

    Update paypal.js

--- server/helpers/paypal.js
+++ server/helpers/paypal.js
@@ -2,8 +2,8 @@ const paypal = require("paypal-rest-sdk");
 
 paypal.configure({
   mode: "sandbox",
-  client_id: "ASvYhKrgdqXnyErQJ0vvORe9I-3ucZOYTuJeA99891RRt-PEqm6uRzgR-BEtLb2xnVaZXc_sI0Ww6147",
-  client_secret: "EPTi6NhW7s5S0Qnm0YKweQRbZ2LSHnhxrvgKpU786fkxpWIiaVky-j6BiHabaaIwgS1Tye85yekoU0PN",
+   client_id: process.env.PAYPAL_ID,
+  client_secret: process.env.PAYPAL_KEY,
 });
 
 module.exports = paypal;
