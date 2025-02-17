7adb2c6f69204f58a331d6f6b28942effa94e8fd
k-iishan <kk1085035@gmail.com>
2025-02-17 11:05:22 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.URL,
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
